f43f15b9fa73e8b6765e38ceeac1ea3452653147
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2025-04-28 10:35:20 -0300

    Criado as classes

---
+++ app/src/main/java/ifsc/poo/lampada.java
@@ -0,0 +1,17 @@
+package ifsc.poo;
+
+public class lampada {
+    Boolean ligada;
+
+    public static void ligar(){
+
+    }
+
+    public static void desligar(){
+
+    }
+
+    public static boolean verEstado(){
+        return true;
+    }
+}
