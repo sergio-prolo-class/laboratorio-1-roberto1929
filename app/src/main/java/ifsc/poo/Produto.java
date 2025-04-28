77979d7136440daad9be164f7a82c0e88b0ccbe2
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2025-04-28 14:50:40 -0300

    Criado e editado Produto

--- app/src/main/java/ifsc/poo/Produto.java
+++ app/src/main/java/ifsc/poo/Produto.java
@@ -1,4 +1,27 @@
 package ifsc.poo;
 
-public class produto {
+public class Produto {
+    private String codigo;
+    private String nome;
+    private int preco;
+    private double desconto;
+
+    public Produto(String codigo, String nome, int preco, double desconto) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+    public String setDesconto(double desconto) {
+        this.desconto = desconto;
+        return desconto + "";
+    }
+    public double precoFinal() {
+        return preco * desconto;
+    }
+    public String anuncio() {
+        return nome + " " + preco;
+    }
+
 }
